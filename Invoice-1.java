23dec6de9a97d05438cd3728b3b07f6fc64e6db3
Ezra <ezraogechi697@gmail.com>
2025-11-26 15:12:44 +0300

    Add files via upload

---
+++ Invoice-1.java
@@ -0,0 +1,31 @@
+public class Invoice{
+    public static void main(String[] args){
+        System.out.println("==================================================================================================");
+        System.out.println("Bill To:                                          Ship To:                                        ");
+        System.out.println("==================================================================================================");
+        System.out.println("C026-01-1136/2023                               Ezra Ndinya Ogechi                          ");
+        System.out.println("No 23/2,SBI Colony.                             No 23/2, SBI Colony                               ");
+        System.out.println("Ragavendra Nagar, Chennal - 600124              Ragavendra Nagar,Chennai-600124                   ");
+        System.out.println("GSTIN:33APFSDF1ZV                               GSTIN:33APFSDF1ZV                                 ");
+        System.out.println("Contact:+91-7869825463                          Contact:+91-7869825463                            ");
+        System.out.println("Email:purchase-team@thendral.com                Email:purchase-team@thendral.com                  ");
+        System.out.println("==================================================================================================");
+        System.out.println();
+
+        System.out.println("Payment Date: 7 days from date of delivery      Payment Terms:100% against invoic                 ");
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        System.out.println("| S.No | Product Code | Product Name     | HSN Code | Quantity | Units | Rate   | Tax | Amount   |");
+        System.out.println("--------------------------------------------------------------------------------------------------");
+        System.out.println("|  1   |     105      | Surf Excel 5 kg  | 34019011 |    20    |  nos  | 600.00 | 5%  | 12600.00 |");
+        System.out.println("|  2   |     109      | Rin 1kg          | 34019011 |    25    |  nos  | 85.00  | 5%  | 2231.00  |");
+        System.out.println("|  3   |     115      | Hamam soap 150 g | 34011100 |    25    |  nos  | 60.00  | 5%  | 1575.00  |");
+        System.out.println("|  4   |     105      | Lux Soap 150 g   | 34011100 |    30    |  nos  | 53.00  | 5%  | 443.00   |");
+        System.out.println("|  5   |     105      |  Dove soap 125 g | 34011100 |    25    |  nos  | 75.00  | 5%  | 1968.00  |");
+        System.out.println("|  6   |     105      |  Vim bar 200 g   | 08109010 |    20    |  nos  | 15.00  | 5%  | 315.00   |");
+        System.out.println("|  7   |     105      | Surf Excel 5 kg  | 33061020 |    30    |  nos  | 85 .00 | 18% | 13009.00 |");
+        System.out.println("|                                                     Total:                 22141.00            |");
+        System.out.println("|                                                     Discount:                141.00            |");
+        System.out.println("|                                                     Grand Total            22000.00            |");
+        System.out.println("-------------------------------------------------------------------------------------------------|");
+    }
+}
